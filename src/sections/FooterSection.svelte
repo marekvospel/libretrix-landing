8b2910be4949779c4c68500f286437268c58116f
Marek Vospěl <marek@vospel.cz>
2024-02-10 15:56:21 +0100

    chore: accessibility labels

--- src/sections/FooterSection.svelte
+++ src/sections/FooterSection.svelte
@@ -1,10 +1,10 @@
 
 <footer class="flex flex-col gap-8 items-center pb-8 pt-[15dvh]">
   <div class="w-full max-w-150 flex flex-row flex-wrap justify-between items-center">
-    <a href="https://github.com/marekvospel/libretrix" target="_blank" rel="noreferrer noopener" class="text-text hover:text-text-semilight duration-300 transition-colors">
+    <a href="https://github.com/marekvospel/libretrix" target="_blank" rel="noreferrer noopener" class="text-text hover:text-text-semilight duration-300 transition-colors" aria-label="GitHub">
       <span class="i-bxl:github text-3xl block" />
     </a>
-    <a href="https://app.libretrix.org" target="_blank" rel="noreferrer noopener" class="text-text hover:text-text-semilight duration-300 transition-colors">
+    <a href="https://app.libretrix.org" target="_blank" rel="noreferrer noopener" class="text-text hover:text-text-semilight duration-300 transition-colors" aria-label="App">
       <span class="i-bx:customize text-3xl block" />
     </a>
   </div>
